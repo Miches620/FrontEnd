fcf87f26b1b43803565a512a2d4dc43c6fed879c
Santiago <miches_620@hotmail.com>
2022-05-20 12:26:39 -0300

    Inicio de contruccion del Backend

---
+++ backend/src/main/java/com/miPortfolio/backend/Repositorio/PersonaRepositorio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.miPortfolio.backend.Repositorio;
+
+/**
+ *
+ * @author miche
+ */
+public class PersonaRepositorio {
+    
+}
